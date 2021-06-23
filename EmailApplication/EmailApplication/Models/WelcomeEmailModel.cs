180bd6f6cbff2b4fd9e310849ac159486a601eed
Ahad726 <atmahad726@gmail.com>
2021-06-23 17:55:44 +0600

    Missing cs file added

---
+++ EmailApplication/EmailApplication/Models/WelcomeEmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailApplication.Models
+{
+    public class WelcomeEmailModel
+    {
+        public string InstructorName { get; set; }
+
+        public WelcomeEmailModel(string instructorName)
+        {
+            InstructorName = instructorName;
+        }
+    }
+}
---
+++ EmailApplication/EmailService/IViewRenderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public interface IViewRenderService
+    {
+        Task<string> RenderToStringAsync(string viewPath, object model);
+    }
+}
